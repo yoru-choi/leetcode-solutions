7172b394b1fe12c2cfe6645b5959ffd76d04b966
TEN <qlqjs674@naver.com>
2026-08-21 18:44:51 +0900

    Time: 4 ms (100%), Space: 45.5 MB (96.96%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+                if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        long reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
